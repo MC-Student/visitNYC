d393e160bf5820d01086d5aedcd2d93d808b801a
mtcstudent <47408386+mtcstudent@users.noreply.github.com>
2022-03-24 00:33:18 -0400

    Add files via upload
    Connected files

---
+++ Time.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function showTime() {
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    document.getElementById("timeNow").innerHTML = time.toLocaleString();
+}
+showTime();
+setInterval("showTime()", 1000);
+
+
+/*This code shows what I was trying to do originally - set the time to Manhattan time, no matter where the browser is opened.
+It ended up showing the number of milliseconds since January 1, 1970 etc, and I know why, but I was not able to turn it
+back into a time. I spent a couple of hours trying to figure it out. In the end, I just decided to have it show the
+current time since it's being opened from NY anyways.
+
+function Time() {
+    var d = new Date();
+    var localTime = d.getTime();
+    var localOffset = d.getTimezoneOffset() * 60000;
+    var utc = localTime + localOffset;
+    var time = utc + (3600000 * -5);
+    document.getElementById("timeNow").innerHTML = "The local time in Manhattan is" +
+        time.toLocaleString("en-US",{dateStyle: "full"});
+}
+
+Time();
+setInterval("Time()", 1000);*/
+
+
